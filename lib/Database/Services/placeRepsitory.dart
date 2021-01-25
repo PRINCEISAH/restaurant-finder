d771ce2cb6d9e0521811677cffa36fb9c69360a2
isah <princeisah15@gmail.com>
2021-01-25 17:24:56 -0500

    getplace

--- lib/Database/Services/placeRepsitory.dart
+++ lib/Database/Services/placeRepsitory.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:html';
 
 import 'package:geolocator/geolocator.dart';
 import 'package:restaurant/Database/Services/service.dart';
@@ -11,7 +12,10 @@ class PlacesRepository {
 
   static List<PlaceDetail> placedetails = [];
 
-  Future<List<PlaceDetail>> places() async {
+  final String detailUrl =
+      "https://maps.googleapis.com/maps/api/place/details/json?key=${PLACES_API_Key}&placeid=";
+
+  Future<List<PlaceDetail>> placesapi() async {
     Position position = await _userLocation.getCurrentLocation();
 
     var url =
@@ -27,5 +31,24 @@ class PlacesRepository {
     print(places);
 
     return places;
+  }
+
+  Future getPlace(String place_id) async {
+    var response = await http
+        .get(detailUrl + place_id, headers: {"Accept": "application/json"});
+    var result = json.decode(response.body)["result"];
+
+    List<String> weekdays = [];
+    if (result["opening_hours"] != null)
+      weekdays = result["opening_hours"]["weekday_text"];
+    return new PlaceDetail(
+        result["place_id"],
+        result["name"],
+        result["icon"],
+        result["rating"].toString(),
+        result["vicinity"],
+        result["formatted_address"],
+        result["international_phone_number"],
+        weekdays);
   }
 }
